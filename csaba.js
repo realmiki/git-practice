51366b392ceb1934e35a73484ac238b7cbcf8b8f
Elek Balazs <elek.balazs86@gmail.com>
2022-12-05 14:29:18 +0100

    a7

--- csaba.js
+++ csaba.js
@@ -5,3 +5,6 @@ let parameter = Fradi
 let cheer = (parameter) => {
     console.log('our team is called:'+ parameter + "Javascript");
 }
+let cheer2 = (parameter) => {
+    console.log('our team is called:'+ parameter + "Javascript");
+}
